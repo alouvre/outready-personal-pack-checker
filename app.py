89485dba8df46d6bd15e5914e94a5950985f3337
Alifia Mustika Sari <alifiamustika02@gmail.com>
2025-07-22 18:34:51 +0700

    Perubahan Proyek Part 3 - penambahan panel kontrol kiri untuk integrasi info setting dan status capture, serta refactor proses pengambilan dan penyimpanan gambar
    🎯 Ringkasan:
    
    Pada tahap ini dilakukan penyusunan ulang arsitektur GUI, serta penyempurnaan proses pengambilan gambar, dengan menambahkan panel kiri sebagai pusat informasi dan integrasi kontrol.
    
    🔧 1. Penambahan Panel Kontrol Kiri (Information & Status Panel)
    
    Panel kiri (control_panel_frame) kini memuat:
    - Informasi pengaturan aktif: kamera ID, mode mirror, resolusi, brightness
    - Status proses pengambilan gambar secara real-time (count, delay, path simpan, dll)
    
    Tujuan: menyediakan pusat kendali dan informasi setting agar pengguna dapat memantau dan mengubah konfigurasi dengan lebih mudah.
    
    🔗 2. Integrasi Panel Lain ke Panel Kontrol
    
    Input setting dari:
    - Panel atas (kamera, mode, resolusi)
    - Panel kanan (brightness)
    - Popup capture. kini dihubungkan dan disinkronisasi ke panel kiri untuk konsistensi informasi.
    
    💾 3. Refactor Proses Pengambilan dan Penyimpanan Gambar
    
    Revisi pada alur fungsi capture_images():
    - Logika penyimpanan file diperbarui agar lebih sistematis dan tahan error
    - Informasi path dan progres pengambilan ditampilkan di panel kiri
    
    Tujuan: membuat proses batch capture lebih terkontrol dan transparan.
    
    🖼️ 4. Penyusunan Ulang Layout agar Lebih Responsif & Scalable
    
    Struktur GUI diatur ulang:
    - Panel kiri sebagai anchor control utama
    - Frame kamera sebagai pusat visual
    - Komponen dirapikan agar mendukung skala aplikasi lebih lanjut
    
    Memungkinkan penambahan fitur lanjutan (misalnya: anotasi live, filter kamera, zoom) tanpa membuat antarmuka kacau.
    
    📌 Dampak:
    Perubahan ini mempersiapkan OutReady menuju aplikasi dataset builder yang lebih tangguh dan siap dikembangkan ke skala produksi — baik dari sisi usability, maintainability, maupun extensibility.

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from tkinter import messagebox
 from PIL import Image
 from datetime import datetime
 
-# ==== Konfigurasi awal ====
+# ---------------------------------------------------
+# --------------- Konfigurasi Awal ------------------
+# ---------------------------------------------------
 SAVE_DIR = "dataset/raw"
 MODES = ["Default", "Mirror"]
 RESOLUTIONS = {
@@ -27,35 +29,61 @@ ctk.set_appearance_mode("light")
 ctk.set_default_color_theme("blue")
 
 
-# ==== GUI ====
+# ---------------------------------------------------
+# --------------------- GUI -------------------------
+# ---------------------------------------------------
 class CameraApp:
     """Aplikasi GUI pengambilan gambar dengan webcam."""
     def __init__(self, root):
         self.root = root
-        self.root.geometry("1080x720")
+        # Hitung ukuran layar
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
 
-        # State
+        # Ukuran proporsional
+        window_width = int(screen_width * 0.85)
+        window_height = int(screen_height * 0.85)
+
+        # Posisi tengah
+        x = (screen_width // 2) - (window_width // 2)
+        y = (screen_height // 2) - (window_height // 2)
+
+        self.root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+        # State Global
+        self.selected_camera_id = ctk.StringVar(value="0")
         self.selected_mode = ctk.StringVar(value=MODES[1])
         self.selected_resolution = ctk.StringVar(value="640x480")
-        self.selected_camera_id = ctk.StringVar(value="0")
         self.available_camera_ids = detect_available_cameras()
         self.brightness_value = ctk.IntVar(value=0)
+
         self.current_camera_id = 0
+        self.window_width = int(screen_width * 0.85)
+        self.setting_info_label = None
 
         # Inisialisasi kamera
         self.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
         self.set_resolution("640x480")
         self.is_updating = True  # flag untuk pause/resume update_frame
 
+        # Inisialisasi nilai default untuk settingan pengambilan gambar
+        self.capture_settings = {
+            "count": 1,
+            "delay": 2,
+        }
+
         self.root.title("OutReady - Capture Image")
         self.build_ui()
         self.update_frame()
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-    # Bangun UI
+    # -------------------------------------------
+    # --------------- Build UI ------------------
+    # -------------------------------------------
     def build_ui(self):
         self.build_top_controls()
         self.build_right_controls()
+        self.build_left_controls()  # Label baru dibangun terakhir
         self.build_canvas()
         self.build_buttons()
 
@@ -64,15 +92,18 @@ class CameraApp:
     # -------------------------------------------
     def build_top_controls(self):
         """Bangun panel kontrol atas secara modular dan scalable."""
+        # Buat frame utama panel atas
         self.top_controls_frame = ctk.CTkFrame(self.root)
         self.top_controls_frame.pack(padx=10, pady=10, fill="x")
 
+        # Builder section terpisah
         self.top_feature_builders = [
             self.build_camera_id_selector,
             self.build_camera_mode_control,
             self.build_resolution_selector,
         ]
 
+        # Bangun tiap section di dalam panel atas
         for builder in self.top_feature_builders:
             section_frame = ctk.CTkFrame(
                 self.top_controls_frame,
@@ -86,9 +117,14 @@ class CameraApp:
     # -------------------------------------------
     def build_right_controls(self):
         """Bangun panel kontrol kanan secara modular dan scalable."""
-        self.right_controls_frame = ctk.CTkFrame(self.root)
+        # Hitung 20% dari lebar jendela untuk panel kanan (kamu sudah simpan ke self.window_width sebelumnya)
+        right_width = int(self.window_width * 0.20)
+
+        # Buat frame utama panel kanan
+        self.right_controls_frame = ctk.CTkFrame(self.root, width=right_width)
         self.right_controls_frame.pack(side="right", padx=10, pady=10, fill="y")
 
+        # Builder section terpisah
         self.feature_builders = [
             self.build_brightness_control,
             # Tambahkan fungsi-fungsi kontrol lain di sini
@@ -96,14 +132,40 @@ class CameraApp:
             # self.build_zoom_control,
         ]
 
+        # Bangun tiap section di dalam panel kanan
         for builder in self.feature_builders:
             section_frame = ctk.CTkFrame(self.right_controls_frame)
             section_frame.pack(pady=10, fill="x", padx=5)
             builder(section_frame)
 
-# -------------------------------------------
-# --------------- Panel Utama ---------------
-# -------------------------------------------
+    # -------------------------------------------
+    # ----------- Panel Kontrol Kiri ------------
+    # -------------------------------------------
+    def build_left_controls(self):
+        """Bangun panel kontrol kiri secara modular dan scalable."""
+        # Hitung 20% dari lebar jendela untuk panel kiri (kamu sudah simpan ke self.window_width sebelumnya)
+        left_width = int(self.window_width * 0.20)
+
+        # Buat frame utama panel kiri
+        self.left_controls_frame = ctk.CTkFrame(self.root, width=left_width)
+        self.left_controls_frame.pack(side="left", padx=10, pady=10, fill="y")
+
+        # Builder section terpisah
+        self.feature_builders = [
+            # Tambahkan fungsi-fungsi kontrol lain di sini
+            self.build_setting_info_section,
+        ]
+
+        # Bangun tiap section di dalam panel kiri
+        for builder in self.feature_builders:
+            section_frame = ctk.CTkFrame(self.left_controls_frame)
+            section_frame.pack(pady=10, fill="x", padx=5)
+            section_frame.pack_propagate(False)
+            builder(section_frame)
+
+# -----------------------------------------------
+# ----------------- Panel Utama -----------------
+# -----------------------------------------------
     def build_canvas(self):
         """Bangun area tampilan kamera."""
         self.image_label = ctk.CTkLabel(self.root, text="")
@@ -111,17 +173,32 @@ class CameraApp:
 
     def build_buttons(self):
         """Bangun tombol kontrol bawah."""
-        self.capture_btn = ctk.CTkButton(
-            self.root,
-            text="Take Picture",
+        button_frame = ctk.CTkFrame(self.root, fg_color="transparent")
+        button_frame.pack(pady=10)
+
+        self.start_btn = ctk.CTkButton(
+            button_frame,
+            text="Start",
             font=("Arial", 14),
-            # command=self.capture_images,
-            command=self.open_capture_popup,
+            # command=self.capture_loop,
+            command=self.start_capture_process,
             corner_radius=32,
             height=40,
-            width=35
+            width=150
         )
-        self.capture_btn.pack(pady=10)
+        self.start_btn.pack(side="left", padx=10, expand=True)
+
+        self.capture_setting_btn = ctk.CTkButton(
+            button_frame,
+            text="Capture Setting",
+            font=("Arial", 14),
+            # command=self.capture_loop,
+            command=self.open_capture_setting_popup,
+            corner_radius=32,
+            height=40,
+            width=150
+        )
+        self.capture_setting_btn.pack(side="left", padx=10, expand=True)
 
     def update_frame(self):
         """Ambil frame dari kamera, olah, dan tampilkan."""
@@ -160,29 +237,49 @@ class CameraApp:
         brightness = self.brightness_value.get()
         return cv2.convertScaleAbs(frame, alpha=1.0, beta=brightness)
 
-    def capture_images(self, count=1, delay=0.2):
+    def start_capture_process(self):
+        """Inisiasi proses capture burst dengan indikator visual non-blocking."""
+        self.image_index = 0
+        self.total_images = self.capture_settings["count"]
+        self.delay_ms = int(self.capture_settings["delay"] * 1000)  # dalam milidetik
+        self.capture_loop()
+
+    def capture_loop(self):
         """Mengambil sejumlah gambar dengan jeda tertentu untuk kebutuhan dataset burst."""
-        try:
+        """Loop pengambilan gambar secara non-blocking + indikator visual."""
+        # try:
+        if self.image_index < self.total_images:
+            ret, frame = self.cap.read()
+            if not ret:
+                messagebox.showerror("Gagal", "Tidak dapat mengambil gambar.")
+                return
+
+            # Simpan gambar
             save_dir = os.path.join(SAVE_DIR)
             os.makedirs(save_dir, exist_ok=True)
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            filename = f"image_{timestamp}.jpg" # Tambahkan settingan milih jpg atau png atau yang lain
+            path = os.path.join(SAVE_DIR, filename)
+            cv2.imwrite(path, self.current_frame)
 
-            for i in range(count):
-                ret, frame = self.cap.read()
-                if not ret:
-                    messagebox.showerror("Gagal", "Tidak dapat mengambil gambar.")
-                    break
+            # ✅ Update label info capture (misalnya di panel kiri)
+            if hasattr(self, "setting_info_label"):
+                self.setting_info_label.configure(
+                    text=f"Mengambil gambar {self.image_index + 1}/{self.total_images}"
+                )
 
-                timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-                filename = f"image_{timestamp}.jpg"
-                path = os.path.join(SAVE_DIR, filename)
+            self.image_index += 1
 
-                cv2.imwrite(path, self.current_frame)
+            # ⏳ Delay antar capture, non-blocking
+            self.root.after(self.delay_ms, self.capture_loop)
 
-                messagebox.showinfo("[INFO]", "Pengambilan Gambar Berhasil")
-                time.sleep(delay)
+        else:
+            # ✅ Setelah selesai ambil semua gambar
+            if hasattr(self, "setting_info_label"):
+                self.setting_info_label.configure(text="Pengambilan selesai ✅")
 
-        except Exception as e:
-            messagebox.showerror("[ERROR]", f"Terjadi kesalahan saat menyimpan gambar.\n{e}")
+        # except Exception as e:
+        #     messagebox.showerror("[ERROR]", f"Terjadi kesalahan saat menyimpan gambar.\n{e}")
 
     # ------------------------------------------------------
     # ----------- Fitur-Fitur Panel Kontrol Atas -----------
@@ -195,7 +292,8 @@ class CameraApp:
             font=("Arial", 12)
             ).pack(anchor="w")
         self.mode_menu = ctk.CTkOptionMenu(
-            parent, values=MODES, variable=self.selected_mode
+            parent, values=MODES, variable=self.selected_mode,
+            command=lambda _: self.update_setting_info_display()
         )
         self.mode_menu.pack()
 
@@ -236,6 +334,7 @@ class CameraApp:
             self.current_camera_id = selected_id
 
             self.is_updating = True  # resume update
+            self.update_setting_info_display()
 
         except Exception as e:
             messagebox.showerror(
@@ -279,11 +378,12 @@ class CameraApp:
                 self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
             self.is_updating = True
+            self.update_setting_info_display()
         except Exception as e:
             messagebox.showerror("Gagal Mengatur Resolusi", str(e))
 
     # ------------------------------------------------------
-    # ----------- Fitur-Fitur Panel Kontrol Kanan -----------
+    # ----------- Fitur-Fitur Panel Kontrol Kanan ----------
     # ------------------------------------------------------
     def build_brightness_control(self, parent):
         """Bangun kontrol brightness dalam frame terpisah."""
@@ -295,6 +395,7 @@ class CameraApp:
             label_row,
             text="Brightness",
             font=("Arial", 12),
+            anchor="w"
             ).grid(row=0, column=0, sticky="w")
 
         # Label kanan (dalam kotak)
@@ -335,16 +436,60 @@ class CameraApp:
     def update_brightness_label(self, value):
         """Update label nilai brightness saat slider digeser."""
         self.brightness_label.configure(text=f"{int(float(value))}")
+        self.update_setting_info_display()
 
     def reset_brightness(self):
         """Reset nilai brightness ke default."""
         self.brightness_value.set(0)
         self.update_brightness_label(0)
 
-# ------------------------------------------------------
-# ------------------- Pop up Capture -------------------
-# ------------------------------------------------------
-    def open_capture_popup(self):
+    # ------------------------------------------------------
+    # ----------- Fitur-Fitur Panel Kontrol Kiri -----------
+    # ------------------------------------------------------
+    def build_setting_info_section(self, parent):
+        """Panel info pengaturan pengambilan gambar."""
+        label_row = ctk.CTkFrame(parent, fg_color="transparent")
+        label_row.pack(fill="x", padx=5, pady=(0, 5))
+
+        # Judul section
+        title = ctk.CTkLabel(
+            label_row,
+            text="Information Setting",
+            font=("Arial", 12, "bold"),
+            anchor="w")
+        title.pack(fill="x", pady=(0, 5))
+
+        # Label info setting (multiline)
+        self.setting_info_label = ctk.CTkLabel(
+            label_row,
+            text=self.get_capture_setting_text(),
+            font=("Arial", 12),
+            justify="left",
+            anchor="w"
+        )
+        self.setting_info_label.pack(fill="x", pady=(0, 5))
+
+    def get_capture_setting_text(self):
+        return (
+            f"Total Image: {self.capture_settings['count']}\n"
+            f"Delay: {self.capture_settings['delay']} s\n"
+            f"Camera ID: {self.selected_camera_id.get()}\n"
+            f"Mode: {self.selected_mode.get()}\n"
+            f"Resolution: {self.selected_resolution.get()}\n"
+            f"Brightness: {self.brightness_value.get()}\n"
+        )
+
+    def update_setting_info_display(self):
+        try:
+            self.setting_info_label.configure(text=self.get_capture_setting_text())
+        except AttributeError:
+            # Label belum dibangun
+            pass
+
+# --------------------------------------------------------------
+# ------------------- Pop up Capture Setting -------------------
+# --------------------------------------------------------------
+    def open_capture_setting_popup(self):
         if not self.cap or not self.cap.isOpened():
             messagebox.showerror(
                 "Kamera Tidak Aktif",
@@ -353,11 +498,11 @@ class CameraApp:
             return
 
         # Pause update frame sementara
-        self.is_updating = False
+        self.is_updating = True
 
         popup = ctk.CTkToplevel(self.root)
         popup.title("Pengaturan Capture Gambar")
-        popup.geometry("640x480")
+        popup.geometry("420x250")
         popup.transient(self.root)         # Supaya tetap di depan
         popup.grab_set()                   # Modal: tidak bisa interaksi jendela utama
         popup.focus_force()
@@ -376,29 +521,32 @@ class CameraApp:
         # Frame utama
         content = ctk.CTkFrame(popup)
         content.pack(expand=True, fill="both", padx=20, pady=20)
+        content.grid_columnconfigure(1, weight=1)
 
-        # Jumlah burst
+        # INPUT : Jumlah Gambar
         ctk.CTkLabel(
             content,
-            text="Jumlah Gambar:",
+            text="Total Image:",
             font=("Arial", 12),
-            anchor="w").pack(fill="x", pady=(10, 0), padx=10)
+            anchor="w").grid(row=0, column=0, sticky="w", padx=5, pady=(5, 0))
         count_entry = ctk.CTkEntry(content)
         count_entry.insert(0, "1")
-        count_entry.pack(fill="x", pady=0, padx=5)
 
-        # Delay antar gambar
+        count_entry.grid(row=0, column=1, sticky="ew", padx=5, pady=(5, 0))
+
+        # INPUT : Delay antar gambar
         ctk.CTkLabel(
             content,
-            text="Delay (detik):",
+            text="Delay (seconds):",
             font=("Arial", 12),
-            anchor="w").pack(fill="x", pady=(10, 0), padx=10)
+            anchor="w").grid(row=1, column=0, sticky="w", padx=5, pady=(10, 0))
+
         delay_entry = ctk.CTkEntry(content)
         delay_entry.insert(0, "0.2")
-        delay_entry.pack(fill="x", pady=0, padx=10)
+        delay_entry.grid(row=1, column=1, sticky="ew", padx=5, pady=(10, 0))
 
-        # Tombol konfirmasi
-        def confirm_capture():
+        # Tombol Simpan
+        def save_settings():
             try:
                 count = max(1, int(count_entry.get()))
             except ValueError:
@@ -412,19 +560,22 @@ class CameraApp:
                 messagebox.showwarning("Jumlah Gambar Kosong", "Mohon masukkan jumlah gambar.")
                 return
 
+            self.capture_settings["count"] = count
+            self.capture_settings["delay"] = delay
+            self.update_setting_info_display()
+
             popup.destroy()
             self.is_updating = True
-            self.capture_images(count, delay)
 
-        # Tombol aksi
-        ctk.CTkButton(
+        save_btn = ctk.CTkButton(
             content,
-            text="Capture",
+            text="Save",
+            command=save_settings,
             font=("Arial", 14),
-            command=confirm_capture,
             corner_radius=32,
-            height=30,
-            width=35).pack(pady=(20, 5))
+            height=32
+        )
+        save_btn.grid(row=2, column=0, columnspan=2, pady=(20, 10))
 
         # Handle saat popup ditutup manual
         def on_close():
